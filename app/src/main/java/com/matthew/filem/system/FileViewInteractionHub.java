cfaeb2575b9ceb57981c22dd0ac9d7d3d49f4661
caoyongren <m18410261910@163.com>
2017-08-30 20:29:14 +0800

    modify FileViewInteractionHub

--- app/src/main/java/com/matthew/filem/system/FileViewInteractionHub.java
+++ app/src/main/java/com/matthew/filem/system/FileViewInteractionHub.java
@@ -19,15 +19,15 @@ import android.view.Window;
 import android.widget.GridView;
 import android.widget.ListView;
 
-import com.matthew.filem.activity.FileManagerPreferenceActivity;
-import com.matthew.filem.activity.base.BaseActivity;
-import com.matthew.filem.activity.MainActivity;
-import com.matthew.filem.fragment.leftbar.CommonRightFragment;
-import com.matthew.filem.info.FileInfo;
-import com.matthew.filem.component.MenuFirstDialog;
 import com.matthew.filem.R;
+import com.matthew.filem.activity.FileManagerPreferenceActivity;
+import com.matthew.filem.activity.MainActivity;
+import com.matthew.filem.activity.base.BaseActivity;
+import com.matthew.filem.component.MenuFirstDialog;
 import com.matthew.filem.component.PropertyDialog;
+import com.matthew.filem.fragment.leftbar.CommonRightFragment;
 import com.matthew.filem.impl.IFileInteractionListener;
+import com.matthew.filem.info.FileInfo;
 import com.matthew.filem.utils.Constants;
 import com.matthew.filem.utils.L;
 import com.matthew.filem.utils.LocalCacheLayout;
@@ -39,6 +39,7 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 public class FileViewInteractionHub implements FileOperationHelper.IOperationProgressListener {
+    private static final String TAG = "FileViewInteractionHub DEBUG::";
     private static final int FILE_NAME_LEGAL = 0;
     private static final int FILE_NAME_NULL = 1;
     private static final int FILE_NAME_ILLEGAL = 2;
@@ -94,14 +95,8 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
     }
 
     public boolean canShowCheckBox() {
-//        mConfirmOperationBar.getVisibility() != View.VISIBLE
         return true;
     }
-//
-//    private void showConfirmOperationBar(boolean show) {
-//        mConfirmOperationBar.setVisibility(show ? View.VISIBLE : View.GONE);
-//    }
-
     public void addDialogSelectedItem(FileInfo fileInfo) {
         mCheckedFileNameList.add(fileInfo);
     }
@@ -205,52 +200,7 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
 
     private void setup() {
         setupFileListView();
-//        setupOperationPane();
     }
-
-//    // buttons
-//    private void setupOperationPane() {
-//        mConfirmOperationBar = mFileViewListener.getViewById(R.id.moving_operation_bar);
-//        setupClick(mConfirmOperationBar, R.id.button_moving_confirm);
-//        setupClick(mConfirmOperationBar, R.id.button_moving_cancel);
-//    }
-
-//    private void setupClick(View v, int id) {
-//        //TODO
-//        View button = (v != null ? v.findViewById(id) : mFileViewListener.getViewById(id));
-//        if (button != null)
-//            button.setOnClickListener(buttonClick);
-//    }
-
-//    private View.OnClickListener buttonClick = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            switch (v.getId()) {
-//                case R.id.button_operation_copy:
-//                    doOnOperationCopy();
-//                    break;
-//                case R.id.button_operation_move:
-//                    onOperationMove();
-//                    break;
-//                case R.id.button_operation_send:
-//                    onOperationSend();
-//                    break;
-//                case R.id.button_operation_delete:
-//                    onOperationDelete();
-//                    break;
-//                case R.id.button_operation_cancel:
-//                    onOperationSelectAllOrCancel();
-//                    break;
-//                case R.id.button_moving_confirm:
-//                    onOperationButtonConfirm();
-//                    break;
-//                case R.id.button_moving_cancel:
-//                    onOperationButtonCancel();
-//                    break;
-//            }
-//        }
-//
-//    };
 
     public void onOperationReferesh() {
         refreshFileList();
@@ -467,11 +417,6 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
 
     public void onOperationCopy(ArrayList<FileInfo> files) {
         mFileOperationHelper.Copy(files);
-//        showConfirmOperationBar(true);
-//        View confirmButton = mConfirmOperationBar.findViewById(R.id.button_moving_confirm);
-//        confirmButton.setEnabled(false);
-        // refresh to hide selected files
-//        refreshFileList();
     }
 
     public void onOperationCopyPath() {
@@ -496,10 +441,6 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
     public void onOperationMove() {
         mFileOperationHelper.StartMove(getSelectedFileList());
         clearSelection();
-//        showConfirmOperationBar(true);
-//        View confirmButton = mConfirmOperationBar.findViewById(R.id.button_moving_confirm);
-//        confirmButton.setEnabled(false);
-        // refresh to hide selected files
         refreshFileList();
         copyOrMoveMode = CopyOrMove.Move;
     }
@@ -508,27 +449,7 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
         clearSelection();
         updateNavigationPane();
         mFileViewListener.onRefreshFileList(mCurrentPath, mFileSortHelper);
-
-        // update move operation button state
-//        updateConfirmButtons();
     }
-
-//    private void updateConfirmButtons() {
-//        if (mConfirmOperationBar.getVisibility() == View.GONE)
-//            return;
-//
-//        Button confirmButton = (Button) mConfirmOperationBar
-//                                        .findViewById(R.id.button_moving_confirm);
-//        int text = R.string.operation_paste;
-//        if (isSelectingFiles()) {
-//            confirmButton.setEnabled(mCheckedFileNameList.size() != 0);
-//            text = R.string.operation_send;
-//        } else if (isMoveState()) {
-//            confirmButton.setEnabled(mFileOperationHelper.canMove(mCurrentPath));
-//        }
-//
-//        confirmButton.setText(text);
-//    }
 
     private void updateNavigationPane() {
         ((MainActivity) mContext).setFilePathBar(mFileViewListener.getDisplayPath(mCurrentPath));
@@ -816,10 +737,6 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
         FileInfo file = getSelectedFileList().get(0);
         if (file == null)
             return;
-
-        //InformationDialog dialog = new InformationDialog(mContext, file, mFileViewListener
-        //        .getFileIconHelper());
-        //dialog.show();
         PropertyDialog propertyDialog = new PropertyDialog(mContext, file.filePath);
         propertyDialog.showDialog();
     }
@@ -839,16 +756,6 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
     }
 
     public void onOperationCompress() {
-//        if (getSelectedFileList().size() == 0) {
-//            return;
-//        }
-//
-//        FileInfo file = getSelectedFileList().get(0);
-//        if (file == null) {
-//            return;
-//        }
-//        CompressDialog compressDialog = new CompressDialog(mContext, file.filePath);
-//        compressDialog.showDialog();
     }
 
     public void onOperationDecompress() {
@@ -943,25 +850,6 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
             Log.e(LOG_TAG, "file does not exist on position:" + position);
             return;
         }
-
-//        if (isInSelection()) {
-//            boolean selected = lFileInfo.Selected;
-//
-////            ImageView checkBox = (ImageView) view.findViewById(R.id.file_checkbox);
-//            LinearLayout ll_grid_item_bg = (LinearLayout) view.findViewById
-//                                                          (R.id.ll_grid_item_bg);
-//            if (selected) {
-//                mCheckedFileNameList.remove(lFileInfo);
-//                ll_grid_item_bg.setSelected(false);
-////                checkBox.setImageResource(R.mipmap.btn_check_off_holo_light);
-//            } else {
-//                mCheckedFileNameList.add(lFileInfo);
-//                ll_grid_item_bg.setSelected(false);
-////                checkBox.setImageResource(R.mipmap.btn_check_on_holo_light);
-//            }
-//            lFileInfo.Selected = !selected;
-//            return;
-//        }
 
         if (!fileInfo.IsDir && doubleTag != null) {
             if ("PickerActivity".equals(mContext.getClass().getSimpleName())) {
@@ -1062,6 +950,7 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
 
     public void clearSelection() {
         if (mCheckedFileNameList.size() > 0) {
+            L.i(TAG, "clearSelection::-- >" + mCheckedFileNameList.size());
             for (FileInfo f : mCheckedFileNameList) {
                 if (f == null) {
                     continue;
@@ -1090,28 +979,10 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
         return true;
     }
 
-//    public void copyFile(ArrayList<FileInfo> files) {
-//        mFileOperationHelper.Copy(files);
-//    }
-//
-//    public void moveFileFrom(ArrayList<FileInfo> files) {
-//        mFileOperationHelper.StartMove(files);
-//        showConfirmOperationBar(true);
-//        updateConfirmButtons();
-//        // refresh to hide selected files
-//        refreshFileList();
-//    }
-
     @Override
     public void onFileChanged(String path) {
         notifyFileSystemChanged(path);
     }
-//
-//    public void startSelectFiles(CommonRightFragment.SelectFilesCallback callback) {
-//        mSelectFilesCallback = callback;
-//        showConfirmOperationBar(true);
-//        updateConfirmButtons();
-//    }
 
     public void MouseScrollAction(MotionEvent event) {
         if (event.getAxisValue(MotionEvent.AXIS_VSCROLL) < 0.0f) {
@@ -1181,209 +1052,4 @@ public class FileViewInteractionHub implements FileOperationHelper.IOperationPro
     public boolean isProtected() {
         return mIsProtected;
     }
-
-//    // menu
-//    private static final int MENU_SEARCH = 1;
-//    //     private static final int MENU_NEW_FOLDER = 2;
-////    MENU_SORT = 3;
-//    private static final int MENU_SORT = 0;
-//    private static final int MENU_SEND = 7;
-//    private static final int MENU_RENAME = 8;
-//    private static final int MENU_DELETE = 9;
-//    private static final int MENU_INFO = 10;
-//    private static final int MENU_SORT_NAME = 11;
-//    private static final int MENU_SORT_SIZE = 12;
-//    private static final int MENU_SORT_DATE = 13;
-//    private static final int MENU_SORT_TYPE = 14;
-//    private static final int MENU_REFRESH = 15;
-//    private static final int MENU_SELECTALL = 16;
-//    private static final int MENU_SETTING = 17;
-//    private static final int MENU_EXIT = 18;
-//
-//    public View.OnCreateContextMenuListener mListViewContextMenuListener
-//                                            = new View.OnCreateContextMenuListener() {
-//        @Override
-//        public void onCreateContextMenu(ContextMenu menu, View v,
-//                                        ContextMenu.ContextMenuInfo menuInfo) {
-//            if (isInSelection() || isMoveState())
-//                return;
-//
-//            AdapterView.AdapterContextMenuInfo info
-//                                               = (AdapterView.AdapterContextMenuInfo) menuInfo;
-//
-//            SubMenu sortMenu = menu.addSubMenu(0, MENU_SORT, 0, R.string.menu_item_sort).setIcon(
-//                    R.drawable.ic_menu_sort);
-//            addMenuItem(sortMenu, MENU_SORT_NAME, 0, R.string.menu_item_sort_name);
-//            addMenuItem(sortMenu, MENU_SORT_SIZE, 1, R.string.menu_item_sort_size);
-//            addMenuItem(sortMenu, MENU_SORT_DATE, 2, R.string.menu_item_sort_date);
-//            addMenuItem(sortMenu, MENU_SORT_TYPE, 3, R.string.menu_item_sort_type);
-//            sortMenu.setGroupCheckable(0, true, true);
-//            sortMenu.getItem(0).setChecked(true);
-//
-//            addMenuItem(menu, Constants.MENU_COPY, 0, R.string.operation_copy);
-//            addMenuItem(menu, Constants.MENU_COPY_PATH, 0, R.string.operation_copy_path);
-//            addMenuItem(menu, Constants.MENU_PASTE, 0,
-//                    R.string.operation_paste);
-//            addMenuItem(menu, Constants.MENU_MOVE, 0, R.string.operation_move);
-//            addMenuItem(menu, MENU_SEND, 0, R.string.operation_send);
-//            addMenuItem(menu, MENU_RENAME, 0, R.string.operation_rename);
-//            addMenuItem(menu, MENU_DELETE, 0, R.string.operation_delete);
-//            addMenuItem(menu, MENU_INFO, 0, R.string.operation_info);
-//            addMenuItem(menu, Constants.MENU_NEW_FOLDER, 0, R.string.operation_folder);
-//            if (!canPaste()) {
-//                MenuItem menuItem = menu.findItem(Constants.MENU_PASTE);
-//                if (menuItem != null)
-//                    menuItem.setEnabled(false);
-//            }
-//        }
-//    };
-//
-//    private void addMenuItem(Menu menu, int itemId, int order, int string) {
-//        addMenuItem(menu, itemId, order, string, -1);
-//    }
-//
-//    private void addMenuItem(Menu menu, int itemId, int order, int string, int iconRes) {
-//        if (!mFileViewListener.shouldHideMenu(itemId)) {
-//            MenuItem item = menu.add(0, itemId, order, string)
-//                                .setOnMenuItemClickListener(menuItemClick);
-//            if (iconRes > 0) {
-//                item.setIcon(iconRes);
-//            }
-//        }
-//    }
-//
-//    private MenuItem.OnMenuItemClickListener menuItemClick
-//                                             = new MenuItem.OnMenuItemClickListener() {
-//
-//        @Override
-//        public boolean onMenuItemClick(MenuItem item) {
-//            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
-//                                                      item.getMenuInfo();
-//            mListViewContextMenuSelectedItem = info != null ? info.position : -1;
-//
-//            int itemId = item.getItemId();
-//            if (mFileViewListener.onOperation(itemId)) {
-//                return true;
-//            }
-//
-//            addContextMenuSelectedItem();
-//
-//            switch (itemId) {
-//                case Constants.MENU_NEW_FOLDER:
-//                    onOperationCreateFolder();
-//                    break;
-//                case MENU_REFRESH:
-//                    onOperationReferesh();
-//                    break;
-//                case MENU_SELECTALL:
-//                    onOperationSelectAllOrCancel();
-//                    break;
-//                case Constants.MENU_SHOWHIDE:
-//                    onOperationShowSysFiles();
-//                    break;
-//                case MENU_SETTING:
-//                    onOperationSetting();
-//                    break;
-//                case MENU_EXIT:
-//                    ((MainActivity) mContext).finish();
-//                    break;
-//                // sort
-//                case MENU_SORT_NAME:
-//                    item.setChecked(true);
-//                    onSortChanged(FileSortHelper.SortStyle.name);
-//                    break;
-//                case MENU_SORT_SIZE:
-//                    item.setChecked(true);
-//                    onSortChanged(FileSortHelper.SortStyle.size);
-//                    break;
-//                case MENU_SORT_DATE:
-//                    item.setChecked(true);
-//                    onSortChanged(FileSortHelper.SortStyle.date);
-//                    break;
-//                case MENU_SORT_TYPE:
-//                    item.setChecked(true);
-//                    onSortChanged(FileSortHelper.SortStyle.type);
-//                    break;
-//
-//                case Constants.MENU_COPY:
-//                    doOnOperationCopy();
-//                    break;
-//                case Constants.MENU_COPY_PATH:
-//                    onOperationCopyPath();
-//                    break;
-//                case Constants.MENU_PASTE:
-//                    onOperationPaste();
-//                    break;
-//                case Constants.MENU_MOVE:
-//                    onOperationMove();
-//                    break;
-//                case MENU_SEND:
-//                    onOperationSend();
-//                    break;
-//                case MENU_RENAME:
-//                    onOperationRename();
-//                    break;
-//                case MENU_DELETE:
-//                    onOperationDelete();
-//                    break;
-//                case MENU_INFO:
-//                    onOperationInfo();
-//                    break;
-//                default:
-//                    return false;
-//            }
-//
-//            mListViewContextMenuSelectedItem = -1;
-//            return true;
-//        }
-//    };
-//
-//    private int mListViewContextMenuSelectedItem;
-//
-//    public void addContextMenuSelectedItem() {
-//        if (mCheckedFileNameList.size() == 0) {
-//            int pos = mListViewContextMenuSelectedItem;
-//            if (pos != -1) {
-//                FileInfo fileInfo = mFileViewListener.getItem(pos);
-//                if (fileInfo != null) {
-//                    mCheckedFileNameList.add(fileInfo);
-//                }
-//            }
-//        }
-//    }
-//
-//    public void onListItemClick(AdapterView<?> parent, View view, int position, long id) {
-//        FileInfo lFileInfo = mFileViewListener.getItem(position);
-//
-//        if (lFileInfo == null) {
-//            Log.e(LOG_TAG, "file does not exist on position:" + position);
-//            return;
-//        }
-//
-//        if (isInSelection()) {
-//            boolean selected = lFileInfo.Selected;
-//            ImageView checkBox = (ImageView) view.findViewById(R.id.file_checkbox);
-//            if (selected) {
-//                mCheckedFileNameList.remove(lFileInfo);
-//                checkBox.setImageResource(R.mipmap.btn_check_off_holo_light);
-//            } else {
-//                mCheckedFileNameList.add(lFileInfo);
-//                checkBox.setImageResource(R.mipmap.btn_check_on_holo_light);
-//            }
-//            lFileInfo.Selected = !selected;
-//            return;
-//        }
-//
-//        if (!lFileInfo.IsDir) {
-//            if (mCurrentMode == Mode.Pick) {
-//                mFileViewListener.onPick(lFileInfo);
-//            } else {
-//                viewFile(lFileInfo);
-//            }
-//            return;
-//        }
-//
-//        mCurrentPath = getAbsoluteName(mCurrentPath, lFileInfo.fileName);
-//        refreshFileList();
-//    }
 }
